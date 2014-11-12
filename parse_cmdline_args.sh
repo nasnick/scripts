580123f2518752a038bd029d1589d9e860669ac2
nasnick <nas.nick@gmail.com>
2014-11-13 11:17:38 +1300

    Update parse_cmdline_args.sh

--- parse_cmdline_args.sh
+++ parse_cmdline_args.sh
@@ -1,30 +1,37 @@
-#!/bin/bash
+#!/bin/bash -x
+set -e
+
 #Nick Schofield 11-11-2014
 
-#Usage: ./deploy.sh <folder_containing_script>
-#e.g. ./deploy.sh RTL_5_3_0_D124
+#Usage: ./deploy.sh <folder_containing_script> <host>
+#e.g. ./deploy.sh RTL_5_3_0_D124 0001-db-prod-ndc
+
+remote_db_env=$RD_OPTION_REMOTE_DB_ENV
 
 #variables
-RTL_DIR=$1
+SCRIPT_PATH="/var/rundeck/projects/RTL-Deployment/rtl-update-scripts"
+RTL_DIR=$SCRIPT_PATH/$1
 INCR=1
 
 #cd into script directory
 cd $RTL_DIR
 
-#A count of how many files 
+#A count of how many files
 NUMBER_OF_FILES=`ls | wc -l`
 
 #While loop to run scripts in order
-while [ $NUMBER_OF_FILES -gt 0 ]
+while [ $NUMBER_OF_FILES -ne 0 ]
   FILE=`ls | grep ^0$INCR.*pgs$`
 
 #Run script
- do echo "nohup pgScript -h localhost -d process_activity -U postgres -e ansi $FILE 2> $FILE.log &"
+ do /usr/bin/pgScript -h $2 -d process_activity -U postgres -e ansi $FILE 2> $FILE.log
+ sleep 5
 
 #Check for errors in log
-grep ERROR $FILE.log
-  if [ $? -eq 0 ]; then
-    echo "Error in script. Contact author."
+ERROR="$(grep ERROR $FILE.log | wc -l)"
+if [ $ERROR -gt 0 ]; then
+  ERROR_LOG="$(grep ERROR 02-TeamStreetAddress.pgs.log | sed -e 's/^[ \t]*//')"
+  echo "Error in script. Contact author. Error= '$ERROR_LOG'"
   exit 1
 fi
 
@@ -32,6 +39,9 @@ fi
 NUMBER_OF_FILES=$((NUMBER_OF_FILES - 1))
 INCR=$((INCR + 1))
 
-sleep 2
+#Break when all scripts are run
+if [ $NUMBER_OF_FILES -eq 0 ]; then
+  break
+fi
 
 done
