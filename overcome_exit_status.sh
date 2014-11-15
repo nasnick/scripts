f32b910d379a6c4dafe6cf8a39d34d4a0a3bc18f
System Administrator <root@nickschofield.local>
2014-11-15 17:03:57 +1300

    Nov 15

--- overcome_exit_status.sh
+++ overcome_exit_status.sh
@@ -26,6 +26,38 @@ echo $?
 if [ $? -eq 0 ]; then
   echo "well is this going to work?"
 fi
+<<<<<<< Local Changes
+
+
+#!/bin/bash
+PATH_TO_LOGS='/var/log'
+LOGS='weekly.out
+authd.log'
+
+grep 'leaving (1) step instbootloader' /var/log/anaconda.log
+if [ $? -eq 0 ]; then
+  echo "well is this going to work?"
+fi
+
+grep 'ronald regan' /var/log/anaconda.log
+if [ $? -eq 0 ]; then
+  echo "you there ronald?"
+  exit 1
+fi
+
+for i in $LOGS; 
+  do grep 'Nov 12 20:21:06' $PATH_TO_LOGS/$i;
+  if [ $? -eq 0 ]; then
+	  echo "this isn't on"
+  sleep 3
+  fi
+done
+
+grep yankee /var/log/weekly.out
+echo $?
+if [ $? -eq 0 ]; then
+  echo "well is this going to work?"
+fi=======
 
 ##################
 
@@ -58,3 +90,4 @@ if [ $ERROR -gt 0 ]; then
   echo "made it to the end no dramas"
 fi
 
+>>>>>>> External Changes
