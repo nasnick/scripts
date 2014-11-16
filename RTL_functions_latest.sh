1c5bbe4a62d389ff41e01046dc96f66841e14e09
nasnick <nas.nick@gmail.com>
2014-11-17 11:18:28 +1300

    Update RTL_functions_latest.sh

--- RTL_functions_latest.sh
+++ RTL_functions_latest.sh
@@ -7,7 +7,7 @@ set -e
 #e.g. ./deploy.sh RTL_5_3_0_D124 0001-db-prod-ndc
 
 #variables
-SCRIPT_PATH="/var/rundeck/projects/RTL-Deployment/rtl-update-scripts"
+SCRIPT_PATH="xxxx"
 RTL_DIR=$SCRIPT_PATH/$1
 INCR=1
 
@@ -32,7 +32,7 @@ function increment(){				#increment counters
 cd $RTL_DIR
 if [ $NUMBER_OF_FILES -ne 0 ]; then
 	FILE=`ls | grep ^0$INCR.*pgs$`
-    do /usr/bin/pgScript -h $2 -d process_activity -U postgres -e ansi $FILE 2>&1 | tee $FILE.log
+    do /usr/bin/pgScript -h $2 -d xxxx_xxxx -U xxxx -e ansi $FILE 2>&1 | tee $FILE.log
     sleep 5
 	  errors
 	  increment
