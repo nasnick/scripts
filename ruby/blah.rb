0bb1a75096971816226f7bbdcc8628fb0ddee0d5
nasnick <nas.nick@gmail.com>
2014-12-11 13:39:38 +1300

    Dec 11

--- ruby/blah.rb
+++ ruby/blah.rb
@@ -1,11 +1,31 @@
-a = []
-f = File.open('blah') do |f|
+@a = []
+f = File.open('bacula.txt') do |f|
   f.lines.each do |line|
-    a << line.split.map()
-end
-end
+   #@a << line.split.map()
+    @a << line
 
-puts a.inspect
+ end
+  puts "file 1: #{@a.size}"
+end
+@b = []
+f = File.open('unxwebp08.txt') do |f|
+  f.lines.each do |line|
+   #@b << line.split.map()
+    @b << line
+    
+    @c = @a - @b
+    @intersection = @a & @b
+ end
+  puts "file 2: #{@b.size}"
+end
+puts "Intersection size: #{@intersection.size}"
+
+puts "These files are not common to both:"
+puts @c
+
+#puts @a.inspect
+#puts @b.inspect
+
 
 #lines = Array.new
 
@@ -14,3 +34,5 @@ puts a.inspect
 #lines.each do |line|
 #  puts line
 #end
+#bacula.txt
+#unxwebp08.txt
