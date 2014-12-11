2e365d182747f9152815f4c836fa425845983531
nasnick <nas.nick@gmail.com>
2014-12-12 10:21:50 +1300

    Update process_scan_results.sh

--- bash/process_scan_results.sh
+++ bash/process_scan_results.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#Renames the lastest PDF downloaded from penetration test and moves it into shared folder.
 
 for i in $(ls -ltr | tail -1 | awk '{print $9}'); do mv $i scan_reports/$1.pdf; done
