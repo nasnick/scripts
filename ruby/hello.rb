f75bd4bd89345d6ce3bcea1d576ebe934558e5c5
nasnick <nas.nick@gmail.com>
2015-03-11 15:01:54 +1300

    Mar 10th

---
+++ ruby/hello.rb
@@ -0,0 +1,45 @@
+#Take a name - lowercase then spit out a greeting
+
+class HelloThere
+  attr_reader :name, :age
+  def initialize(name, age)
+  @name = name
+  @age = age
+end
+
+def upper
+  puts @name.upcase
+end
+
+def print_greeting
+  puts "Hello #{@name} how are you today? I heard you turned #{@age} the other day."
+  end
+end
+
+
+class MessWithPerson
+  def initialize(title)
+    @title = title
+    @people = []
+  end
+
+  def add_person(a_person)
+    @people << a_person
+  end
+
+  def print_people
+    @people.each do |person|
+      puts "Hello #{person.name} you #{person.age} year old codger."
+    end
+  end
+end
+
+person = HelloThere.new('roger', 24)
+person.print_greeting
+person.upper
+
+bozo = MessWithPerson.new('bozo')
+bozo.add_person(person)
+bozo.print_people
+
+
