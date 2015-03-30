4d1183b201ccc5371b35969d00300e7b3e55f25a
nasnick <nas.nick@gmail.com>
2015-03-31 11:43:41 +1300

    11:43:41 AMfc-3339

---
+++ javascript/basics.js
@@ -0,0 +1,13 @@
+####################
+#FOR LOOPS
+####################
+
+#Iterate through an array
+
+var coffees = ["French Roast", "Colombian", "Kona"];
+for (var i = 0; i < coffees.length; i++) {
+     Runs 5 times, with values of step 0 through 4.
+         console.log(coffees[i]);
+         };
+
+###################
