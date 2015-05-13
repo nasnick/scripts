4aad616f25060b8ecfff11d3a0a97b720263263a
nasnick <nas.nick@gmail.com>
2015-05-13 21:45:00 +1200

    Wed 13 May 2015 21:45:00 NZST

--- js/battleship/battleship.js
+++ js/battleship/battleship.js
@@ -1,2 +1,20 @@
 
-document.write("hello");
+var view = {
+	displayMesssge: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location){
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit"); 
+	},
+	displayMiss: function(location){
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+};
+var a = 0;
+var b = 1;
+view.displayHit('0' + 0);
+view.displayMiss(11);
+view.displayMesssge("Is this thing switched on?");
