ff54d1053115a90e29de7a5c3b22629b8dd59745
nasnick <nas.nick@gmail.com>
2015-05-24 19:27:27 +1200

    Sun 24 May 2015 19:27:27 NZST

--- js/flapper_news/app.js
+++ js/flapper_news/app.js
@@ -1,16 +1,27 @@
 angular.module('flapperNews', [])
+.factory('posts', [function() {
+  var o = {
+  	posts: []
+  };
+  return o;
+}])
+
+angular.module('flapperNews', [])
 .controller('MainCtrl', [
 	'$scope',
-	function($scope){
+	'posts',
+	function($scope, posts){
 		$scope.test = 'Hello World!';
 
-$scope.posts = [
-{title: 'post 1', upvotes: 5},
-{title: 'post 2', upvotes: 2},
-{title: 'post 3', upvotes: 15},
-{title: 'post 4', upvotes: 9},
-{title: 'post 5', upvotes: 4}
-];
+$scope.posts = posts.posts;
+
+// [
+// {title: 'post 1', upvotes: 5},
+// {title: 'post 2', upvotes: 2},
+// {title: 'post 3', upvotes: 15},
+// {title: 'post 4', upvotes: 9},
+// {title: 'post 5', upvotes: 4}
+// ];
 
 $scope.addPost = function(){
 	if(!$scope.title || $scope.title === ''){return;}
