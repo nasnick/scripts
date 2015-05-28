d6f13b2a9d10f7319cd31128bca8e1022b3e3950
nasnick <nas.nick@gmail.com>
2015-05-28 22:53:40 +1200

    Thu 28 May 2015 22:53:40 NZST

---
+++ js/angular/script.js
@@ -0,0 +1,8 @@
+function SimpleController($scope) {
+	$scope.customers = [
+			{ name: 'Rangi Heremai', city: 'Invercargill', worth: '2000' }, 
+			{ name: 'Flynn Northam', city: 'Hong Kong' }, 
+			{ name: 'Salooo Rakoo', city: 'Figtown' }
+			];
+}
+
