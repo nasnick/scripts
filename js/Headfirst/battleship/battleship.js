89a62e41e28f9fd3fc4375d2c2d2bccee13b7c45
nasnick <nas.nick@gmail.com>
2015-05-31 16:27:13 +1200

    Sun 31 May 2015 16:27:13 NZST

--- js/Headfirst/battleship/battleship.js
+++ js/Headfirst/battleship/battleship.js
@@ -93,13 +93,27 @@ function parseGuess(guess) {
 		}
 		return null;
 };
-controller.processGuess("A0");
-controller.processGuess("A6"); 
-controller.processGuess("B6"); 
-controller.processGuess("C6");
-controller.processGuess("C4"); 
-controller.processGuess("D4"); 
-controller.processGuess("E4");
-controller.processGuess("B0"); 
-controller.processGuess("B1"); 
-controller.processGuess("B2");
+
+function init(){
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onClick = handleFireButton;
+}
+
+function handleFireButton(){
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = "";
+}
+window.onload = init;
+
+// controller.processGuess("A0");
+// controller.processGuess("A6"); 
+// controller.processGuess("B6"); 
+// controller.processGuess("C6");
+// controller.processGuess("C4"); 
+// controller.processGuess("D4"); 
+// controller.processGuess("E4");
+// controller.processGuess("B0"); 
+// controller.processGuess("B1"); 
+// controller.processGuess("B2");
