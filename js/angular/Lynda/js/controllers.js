267d32108f61618c3d486028822bed5291cae7a2
nasnick <nas.nick@gmail.com>
2015-06-10 21:52:00 +1200

    Wed Jun 10 21:52:00 NZST 2015

--- js/angular/Lynda/js/controllers.js
+++ js/angular/Lynda/js/controllers.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController',[$scope, $http, function($scope, $http) {
+myApp.controller('MyController',['$scope', '$http', function($scope, $http) {
 //create an object
 $http.get('js/data.json').success(function(data) {
   $scope.artists = data;
