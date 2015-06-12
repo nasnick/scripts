6b394f2162050fbc923a163bef4ca1b3bd0368a0
nasnick <nas.nick@gmail.com>
2015-06-13 09:23:07 +1200

    Sat 13 Jun 2015 09:23:07 NZST

--- js/angular/Lynda/js/controllers.js
+++ js/angular/Lynda/js/controllers.js
@@ -1,8 +1,9 @@
-var myApp = angular.module('myApp', []);
+var artistController = angular.module('artistController', []);
 
-myApp.controller('MyController',['$scope', '$http', function($scope, $http) {
-//create an object
+artistController.controller('ListController',['$scope', '$http', function($scope, $http) {
+
 $http.get('js/data.json').success(function(data) {
   $scope.artists = data;
+  $scope.artistOrder = 'name';
   });
 }]);
