e4388b8ba658012612e50c4535fd23b5882b3e57
nasnick <nas.nick@gmail.com>
2015-06-21 18:08:51 +1200

    Sun 21 Jun 2015 18:08:51 NZST

--- js/Lynda/data_driven_app/builds/development/js/controllers/meetings.js
+++ js/Lynda/data_driven_app/builds/development/js/controllers/meetings.js
@@ -1,10 +1,20 @@
 myApp.controller('MeetingsController',
 	function($scope, $firebaseObject) {
-
+		var meetings = '';
 		var ref = new Firebase("https://attendanceapproger.firebaseio.com/meetings");
-		$scope.meetings = $firebaseObject(ref);
-		// $scope.meetings = meetings.$asObject();
-
+		var meetings = $firebaseObject(ref);
+		$scope.meetings = meetings;
+		//addMeeting below is from ng-submit="addMeeting()" in meetings.html
+		$scope.addMeeting = function() {
+			//pushing an object into method $push
+			meetings.$push({
+				name: $scope.meetingname,
+				date: Firebase.ServerValue.TIMESTAMP
+				//A promise - once data has been pushed then field will become empty
+			}).then(function() {
+				$scope.meetingname='';
+			});
+		};
 });
 
 
