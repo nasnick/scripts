c4463fc0b0230ae82f323a493bc673a0e4fdc416
nasnick <nas.nick@gmail.com>
2015-06-27 08:39:53 +1200

    Sat 27 Jun 2015 08:39:53 NZST

--- js/Lynda/data_driven_app/builds/development/js/services/authentication.js
+++ js/Lynda/data_driven_app/builds/development/js/services/authentication.js
@@ -20,7 +20,7 @@ var myObject = {
 		return auth.$createUser({
 			email: user.email,
 			password: user.password
-		}); //$authWithPassword
+		}).then(function(regUser)); //$authWithPassword
 	  }, //register
 
 	}; //myObject
