d6b285e106af0f133796a74a1a627b96ea083909
nasnick <nas.nick@gmail.com>
2015-07-31 12:59:43 +1200

    12:59:43 PMfc-3339

---
+++ bash/copy_job.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+
+A=$(find . -maxdepth 1 -type f -not -name ".*" | grep Racoon[2-7])
+
+for i in $A; do
+  md5sum $i >> checksumoriginal.txt
+  cat  checksumoriginal.txt | sort  > checksumoriginalsorted.txt
+  sed 's/.\///g' checksumoriginalsorted.txt > checksumoriginal.txt
+    cp  $i TEST
+done
+
+#echo "hellothere" > TEST/Racoon2.txt
+
+B=`ls TEST`
+for i in $B; do
+   md5sum TEST/$i sort  >> checksum.txt 
+   cat checksum.txt | sort > checksumsorted.txt
+   sed 's/TEST\///g' checksumsorted.txt > checksum.txt
+     cp $i TEST
+done
+
+FILE_DIFF=$(diff -s  checksum.txt checksumoriginal.txt)
+
+DIFF=$(echo "$?")
+
+if [ "$DIFF" = "0" ]; then
+  for i in $A; do
+    rm $i
+  done
+elif [ "$DIFF" != "0" ]; then
+  echo "Checksum didn't match up - check these files: $FILE_DIFF"
+fi
+
+rm checksum*
