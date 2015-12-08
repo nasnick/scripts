a2af28a9a20f452a74a1b3cf7093d3619b9164bc
nasnick <nas.nick@gmail.com>
2015-12-08 16:59:53 +1300

    Notes folder for rh

--- tmp/compare.rb
+++ tmp/compare.rb
@@ -4,7 +4,7 @@ b= []
 a = IO.readlines("bluray.txt")
 b = IO.readlines("backups.txt")
 
-compare = b - a
+compare = a - b 
 puts "The following VMs weren't backed up:" 
 compare.each do |diff|
   puts "#{diff}"
