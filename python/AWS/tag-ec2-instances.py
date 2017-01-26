087cd08c2eb3272519f0e4cee0b1316711804329
nickschofield <nick.schofield@foundryhq.com>
2017-01-27 10:18:05 +1300

    Added function to tag ec2 instances script to filter ec2 instances

--- python/AWS/tag-ec2-instances.py
+++ python/AWS/tag-ec2-instances.py
@@ -26,23 +26,32 @@ with open(config_file, 'r') as f:
     config = json.load(f)
 
 ec2 = boto3.resource('ec2')
-reservations = ec2.instances.all()
 mytags = config['tags']['Inspector']
-
 instance_ids = []
 
 def get_instance_ids():
-	for reservation in reservations:
-	   #print(reservation.id)
-	   instance_ids.append(reservation.id)
 
+      reservations = ec2.instances.all()
+      for reservation in reservations:
+	 #print(reservation.id)
+         instance_ids.append(reservation.id)
+
+def filter_instances():
+   instance_ids = []
+
+   reservations = ec2.instances.filter(
+      Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+   for reservation in reservations:
+      print(reservation.id, reservation.instance_type)
 
 def tag_instances():
-	for instance in instance_ids:
-	   ec2.create_tags(
-	      Resources=[instance],
-	      Tags= mytags
-	      )
 
-get_instance_ids()
-tag_instances()
+      for instance in instance_ids:
+         ec2.create_tags(
+         Resources=[instance],
+         Tags= mytags
+         )
+
+#get_instance_ids()
+#tag_instances()
+filter_instances()
